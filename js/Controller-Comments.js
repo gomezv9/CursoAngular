a60c726211134ba5aeefec4d48e9c964c0d154a2
Jaime Gómez <gomezv9@hotmail.com>
2016-04-13 11:02:30 -0500

    submit form comentarios

--- js/Controller-Comments.js
+++ js/Controller-Comments.js
@@ -5,10 +5,23 @@
 	function CommentsController(){
 		
 		this.comments = [];
+		this.comment = {};
 		this.show = false;
 
 		this.toggle = function(){
 			this.show = !this.show;
+		};
+
+		this.anonymousChange = function (){
+			if ( this.comment.anonymous ){
+				this.comment.email = "";
+			}
+		};
+
+		this.addComment = function (){
+			this.comment.date = Date.now();
+			this.comments.push(this.comment);
+			this.comment = {};
 		};
 	}
 
