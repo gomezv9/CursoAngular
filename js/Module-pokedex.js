ab8cb0e7d2a0d13c1d41c650f040e6eda0bc3b6e
Jaime Gomez <gomezv9@hotmail.com>
2016-04-14 21:56:15 -0500

    Mi primera ruta: ng-route y ng-view

--- js/Module-pokedex.js
+++ js/Module-pokedex.js
@@ -1,5 +1,18 @@
 (function (){
 
 	angular.module('pokedex',[]);
+
+	angular.module('pokedex', ['ngRoute'])
+
+	.config(['$routeProvider', function ($routeProvider) {
+
+		$routeProvider
+			.when('/', {
+				templateUrl: 'vistas/pokemon.html',
+				controller: 'PokemonController',
+				controllerAs: 'pkmCtrl'
+			})
+
+	}]);
 	
 })();	
